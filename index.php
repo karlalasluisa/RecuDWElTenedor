66dca15c7b50e44d1063efd7fa67e3f07dd02f91
Karla <karla.lasluisa@gmail.com>
2025-01-03 22:26:57 +0100

    Index.php, EDIT
    Actualizado el index.php para usar el restaurantcontroller. Tambien he implementado la funcionalidad de editar restaurantes

--- index.php
+++ index.php
@@ -1,16 +1,8 @@
-<!DOCTYPE html>
-<!--
-Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php
+
+require_once 'app/controllers/RestaurantController.php';
+
+$controller = new RestaurantController();
+$controller->index();
+
+?>
--- persistence/DAO/RestaurantDAO.php
+++ persistence/DAO/RestaurantDAO.php
@@ -4,18 +4,19 @@ require_once 'persistence/conf/Database.php';
 require_once 'app/models/Restaurant.php';
 
 class RestaurantDAO {
+
     public static function getAll() {
         $db = Database::getInstance();
         $stmt = $db->query("SELECT * FROM restaurant");
         $restaurants = [];
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
             $restaurants[] = new Restaurant(
-                $row['id'],
-                $row['name'],
-                $row['image_url'],
-                $row['price_range'],
-                $row['menu'],
-                $row['category_id']
+                    $row['id'],
+                    $row['name'],
+                    $row['image_url'],
+                    $row['price_range'],
+                    $row['menu'],
+                    $row['category_id']
             );
         }
         return $restaurants;
@@ -25,6 +26,23 @@ class RestaurantDAO {
         $db = Database::getInstance();
         $stmt = $db->prepare("INSERT INTO restaurant (name, image_url, price_range, menu, category_id) VALUES (?, ?, ?, ?, ?)");
         $stmt->execute([$name, $image_url, $price_range, $menu, $category_id]);
+    }
+
+    public static function getById($id) {
+        $db = Database::getInstance();
+        $stmt = $db->prepare("SELECT * FROM restaurant WHERE id = ?");
+        $stmt->execute([$id]);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($row) {
+            return new Restaurant($row['id'], $row['name'], $row['image_url'], $row['price_range'], $row['menu'], $row['category_id']);
+        }
+        return null;
+    }
+
+    public static function update($id, $name, $image_url, $price_range, $menu, $category_id) {
+        $db = Database::getInstance();
+        $stmt = $db->prepare("UPDATE restaurant SET name = ?, image_url = ?, price_range = ?, menu = ?, category_id = ? WHERE id = ?");
+        $stmt->execute([$name, $image_url, $price_range, $menu, $category_id, $id]);
     }
 }
 
