b7a22eb8693137aba142a8625af78d3d4a8091d0
Karla <karla.lasluisa@gmail.com>
2025-01-03 21:59:00 +0100

    Creado modelo y dao de restaurante

--- persistence/DAO/RestaurantDAO.php
+++ persistence/DAO/RestaurantDAO.php
@@ -1,16 +1,31 @@
 <?php
+
 require_once 'persistence/conf/Database.php';
+require_once 'app/models/Restaurant.php';
 
 class RestaurantDAO {
     public static function getAll() {
         $db = Database::getInstance();
         $stmt = $db->query("SELECT * FROM restaurant");
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $restaurants = [];
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $restaurants[] = new Restaurant(
+                $row['id'],
+                $row['name'],
+                $row['image_url'],
+                $row['price_range'],
+                $row['menu'],
+                $row['category_id']
+            );
+        }
+        return $restaurants;
     }
+
     public static function insert($name, $image_url, $price_range, $menu, $category_id) {
         $db = Database::getInstance();
         $stmt = $db->prepare("INSERT INTO restaurant (name, image_url, price_range, menu, category_id) VALUES (?, ?, ?, ?, ?)");
         $stmt->execute([$name, $image_url, $price_range, $menu, $category_id]);
     }
 }
+
 ?>
